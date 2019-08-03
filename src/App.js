a64140117100d9144b48a891b1354cfb451713f6
Ikenna Igwe <ikennaigwe@gmail.com>
2019-08-03 00:23:24 -0700

    basics
    going over the basics

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const greetingTarget = `World`;
+    
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Future Design Works - React Lab</h1>
+            </header>
+            <main>
+                <section>
+                    <img src="https://www.dev.education/images/logo.png" alt="DEV Education" />
+                    <a href="http://www.drigwedds.com" target="_blank">Visit My Dentist</a>
+
+                    <div>
+                        Hello, { greetingTarget } <br />
+                        I'm executing - { (() => console.log('>>> executed!'))() }. <br />
+                        A function was { (() => `executed`)() }
+                    </div>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
