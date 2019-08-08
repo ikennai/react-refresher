6390b8f7a8be9cfd2b5828c649cc7f3763985582
Ikenna Igwe <32691096+ikennai@users.noreply.github.com>
2019-08-08 09:16:20 -0700

    styling
    - Created stylesheet for the home component and added some styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,26 @@
+// Library Imports
 import React, { Component } from 'react';
+
+// Style Imports
+import './Home.css';
 
 class Home extends Component {
     render() {
-        return <h1>I'm Home Component</h1>
+        return (
+            <div className="Home">
+                <h2>Welcome to Future Design Works</h2>
+                <p>This is the home component.</p>
+
+                <button 
+                    style={{
+                        backgroundColor: 'gray',
+                        border: '1px solid black'
+                    }}
+                >
+                    Click me!
+                </button>
+            </div>
+        )
     }
 }
 
