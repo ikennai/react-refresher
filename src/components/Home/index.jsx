d43e0e43ee9fee7a0f83453984c390abc9be774e
Ikenna Igwe <ikennaigwe@gmail.com>
2019-08-19 00:51:00 -0700

    Redux
    Created a Redux Store
    (203 of 569)

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,44 +1,50 @@
 // Library Imports
-import React, { Component } from 'react';
+import React from 'react';
+import { bool } from 'prop-types';
+import { connect } from 'react-redux';
 
 // Style Imports
 import './Home.css';
 
-class Home extends Component {
-    constructor() {
-        super();
+const Home = (props) => {
+    const { isMobile } = props;
 
-        this.state = {
-            name: 'Ikenna'
-        }
-    }
+    return (
+        <div className="Home">
+            <p>Hi! Ikenna here.</p>
 
-    // componentDidMount() {
-    //     setTimeout(() => {
-    //         this.setState({
-    //             name: 'Ikenna Igwe'
-    //         })
-    //     }, 1000);
-    // }
+            <p>This application is a collection of learning apps that I followed through from the React Cookbook. I wanted to start from beginning to end and brush up on my skills using React. The book has been pretty informative so far and I encourage you (if you haven't already) to take a look. Some of the samples are a little dated in areas but nothing has been incredibly hard to follow or modify with the latest updates in the React library / system.</p>
+            <p>While you're here feel free to check out my app <em>Gin Scrummy</em> <small>(comming soon)</small></p>
 
-    render() {
-        return (
-            <div className="Home">
-                <p>Hey, {this.state.name} here.</p>
-                <p>This application is a collection of learning apps that I followed through from the React Cookbook. I wanted to start from beginning to end and brush up on my skills using React. The book has been pretty informative so far and I encourage you (if you haven't already) to take a look. Some of the samples are a little dated in areas but nothing has been incredibly hard to follow or modify with the latest updates in the React library / system.</p>
-                <p>While you're here feel free to check out my app <em>Gin Scrummy</em> <small>(comming soon)</small></p>
+            <p>
+                You are using:
+                <strong>{isMobile ? 'mobile' : 'desktop'}</strong>
+            </p>
 
-                <button
-                    style={{
-                        backgroundColor: 'gray',
-                        border: '1px solid black'
-                    }}
-                >
-                    Click me!
-                </button>
-            </div>
-        )
-    }
+            <button
+                style={{
+                    backgroundColor: 'gray',
+                    border: '1px solid black'
+                }}
+            >
+                Click me!
+            </button>
+        </div>
+    );
+};
+
+Home.propTypes = {
+    isMobile: bool
+};
+
+function mapStateToProps(state) {
+    return {
+        isMobile: state.device.isMobile
+    };
 }
 
-export default Home;
+function mapDispatchToProps() {
+    return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/index.js
+++ src/index.js
@@ -2,20 +2,37 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
-// import App from './components/App';
+
+// Redux Store
+import configureStore from './shared/redux/configureStore';
+
 import * as serviceWorker from './serviceWorker';
 
 // Routes
 import AppRoutes from './routes';
 
-render(
-    <Router>
-        <AppRoutes />
-    </Router>,
-    document.getElementById('root')
-)
-// render(<App />, document.getElementById('root'));
+// Configuring Redux Store
+const store = configureStore(window.initialState);
+
+// DOM
+const rootElement = document.getElementById('root');
+
+// App Wrapper
+const renderApp = (Component) => {
+    render(
+        <Provider store={store}>
+            <Router>
+                <Component />
+            </Router>
+        </Provider>,
+        rootElement
+    );
+};
+
+// Rendering our App
+renderApp(AppRoutes);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
