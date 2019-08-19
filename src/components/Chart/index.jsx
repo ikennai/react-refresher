21a611fc3568fd418788dbb523de304550ce69bd
Ikenna Igwe <ikennaigwe@gmail.com>
2019-08-18 23:33:28 -0700

    Chart component
    Fixed chart component

--- src/components/Chart/index.jsx
+++ src/components/Chart/index.jsx
@@ -3,6 +3,32 @@ import c3 from 'c3';
 import './Chart.css';
 
 class Chart extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            chartType: 'line'
+        };
+
+        this.columns = [
+            ['BTC', 3000, 6000, 10000, 15000, 13000, 11000],
+            ['ETH', 2000, 3000, 5000, 4000, 3000, 940],
+            ['XRP', 100, 200, 300, 500, 400, 300],
+        ];
+    }
+
+    setBarChart = () => {
+        this.setState({
+            chartType: 'bar'
+        });
+    }
+
+    setLineChart = () => {
+        this.setState({
+            chartType: 'line'
+        });
+    }
+
     componentDidMount() {
         // When the comoponent mounts the first time we update the chart.
         this.updateChart();
@@ -17,14 +43,24 @@ class Chart extends Component {
         c3.generate({
             bindto: '#chart',
             data: {
-                columns: this.props.columns,
-                type: this.props.chartType
+                columns: this.columns,
+                type: this.state.chartType
             }
         });
     }
 
     render() {
-        return <div id="chart" />
+        return (
+            <div>
+                <div id="chart" />
+
+                <p>
+                    Chart Type
+                    <button onClick={this.setBarChart}>Bar</button>
+                    <button onClick={this.setLineChart}>Line</button>
+                </p>
+            </div>
+        )
     }
 }
 
